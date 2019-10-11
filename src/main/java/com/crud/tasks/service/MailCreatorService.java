0767e479d315793dab89b2bfbfe7b370400ae758
ganzes <charczuk.arkadiusz@gmail.com>
2019-10-11 02:36:54 +0200

    32.2 Zadanie: Ustawienie pozostałych wartosci

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context contex = new Context();
+        contex.setVariable("message", message);
+        contex.setVariable("tasks_url", "http://localhost:80/crud");
+        contex.setVariable("button", "Visit website");
+        contex.setVariable("admin_name", adminConfig.getAdminName());
+        contex.setVariable("goodbye", "See ya later!");
+        contex.setVariable("admin_company", adminConfig.getAdminCompany());
+        return templateEngine.process("mail/created-trello-card-mail", contex);
+    }
+}
